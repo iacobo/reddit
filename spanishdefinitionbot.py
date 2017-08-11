73f427467151c29b357ea77d6f72ca60fe526489
Jacob <phara0h1@hotmail.com>
2017-08-11 18:55:35 +0100

    Update spanishdefinitionbot.py
    Updated to handle words which exist in both Spanish and English (e.g. a, con, me, sin, tan etc). Provides single comment with both possible interpretations.

--- spanishdefinitionbot.py
+++ spanishdefinitionbot.py
@@ -22,15 +22,16 @@ from bs4 import BeautifulSoup
 
 # Params
 
-debug = 0
-lim = 3 # Hot limit for subreddit posts
+debug = 1
+lim = 10 # Depth limit for subreddit posts
 subreddit_list = ["Spanish",
                   "learnspanish",
                   "spanishhelp",
                   "AP_Spanish",
                   "APSpanish",
                   "BeginnerSpanish",
-                  "LANL_Spanish"]
+                  "LANL_Spanish"
+                  ]
 
 # TODO: multi word phrases unquoted
 
@@ -54,7 +55,6 @@ def grab_word(text):
     words = text.strip().split()
     if '"' in text:
         matches=re.findall(r'\"(.+?)\"',text)
-        # matches is now ['String 1', 'String 2', 'String3']
         return list(match.strip('\"') for match in matches)
     elif " '" in text or "' " in text:
         matches=re.findall(r"\'(.+?)\'",text)
@@ -93,7 +93,7 @@ def grab_definition(word):
     page = requests.get(u"http://www.spanishdict.com/translate/{}".format(word.strip().replace(' ', '%20')))
     soup = BeautifulSoup(page.content, 'html.parser')
     
-    if True == True:
+    try:
         condensed_soup = soup.find_all(class_="dictionary-entry dictionary-neodict")
         quickdefs  = soup.find_all(class_="quickdef")
         results = [c.find_all(True, {'class':['dictionary-neodict-entry-title',
@@ -117,7 +117,7 @@ def grab_definition(word):
         results = results_temp
 
         return results, results2
-    else:
+    except:
         print("Page Status Code: " + str(page.status_code))
         return False
 
@@ -128,15 +128,13 @@ def prettify_definition(results):
         definition = u''
         for item in result:
             if item['class'][-1] == "dictionary-neodict-entry-title":
-                definition += u'\n\n----------\n\n'
-                definition += u'###'
+                definition += u'\n\n----------\n\n###'
                 definition += item.get_text()
                 definition += u'\n\n'
             elif item['class'][-1] == "part_of_speech":
                 try:
                     pos_link = item['href']
-                except:
-                    # In case there is no hyperlink
+                except:    # In case there is no hyperlink
                     pos_link = u'https://www.spanishdict.com'
                 try:
                     pos_title = item['title'].replace(")","\)")
@@ -171,7 +169,7 @@ def prettify_quickdef(quickdefs):
                 text += u"\n\n-------------\n\n"
                 if i == 1:
                     text += u"\n\n&nbsp;\n\n\n&nbsp;\n\n"
-                text += u"#[{}](http://www.spanishdict.com/translate/{})".format(item.get_text(), item.get_text())
+                text += u'#[{}](http://www.spanishdict.com/translate/{})'.format(item.get_text(), item.get_text())
                 # Spacer for second definition eng-esp if exists
                 i += 1
             elif item['class'][-1] == "el":
@@ -210,31 +208,19 @@ if __name__ == "__main__":
             posts_replied_to = f.read()
             posts_replied_to = posts_replied_to.split("\n")
             posts_replied_to = list(filter(None, posts_replied_to))
-        
-    subreddit = reddit.subreddit("pythonforengineers")
+
+    ### REMOVE    
+    #subreddit = reddit.subreddit("pythonforengineers")
 
 # Post to Reddit
 
     if debug == 1:
-        for submission in test_cases:
-            try:
-                words = grab_word(submission)
-                for word in words:
-                    definition, quickdef = grab_definition(word)
-                    if definition == False:
-                        print("ERROR: Could not obtain definition.")
-                    else:
-                        pretty_definition = prettify_definition(definition)
-                        pretty_quickdef = prettify_quickdef(quickdef)
-                        if len(words) > 0:
-                            print(format_reply(pretty_quickdef, pretty_definition))
-            except:
-               print("No useable submissions found.")
-
-    elif debug == 0:
-        for submission in subreddit.hot(limit=lim):
-            if submission.id not in posts_replied_to:
+        for sr in subreddit_list:
+            subreddit = reddit.subreddit(sr)
+            for submission in subreddit.hot(limit=lim):
+            #for submission in test_cases:
                 try:
+                    #words = grab_word(submission)
                     words = grab_word(submission.title)
                     for word in words:
                         definition, quickdef = grab_definition(word)
@@ -244,11 +230,30 @@ if __name__ == "__main__":
                             pretty_definition = prettify_definition(definition)
                             pretty_quickdef = prettify_quickdef(quickdef)
                             if len(words) > 0:
-                                submission.reply(format_reply(pretty_quickdef, pretty_definition))
-                                print("Bot replying to : {}", submission.title)
-                                posts_replied_to.append(submission.id)
+                                print(format_reply(pretty_quickdef, pretty_definition))
                 except:
-                  print("No useable submissions found.")
+                   print("No useable submissions found.")
+
+    elif debug == 0:
+        for sr in subreddit_list:
+            subreddit = reddit.subreddit(sr)
+            for submission in subreddit.hot(limit=lim):
+                if submission.id not in posts_replied_to:
+                    try:
+                        words = grab_word(submission.title)
+                        for word in words:
+                            definition, quickdef = grab_definition(word)
+                            if definition == False:
+                                print("ERROR: Could not obtain definition.")
+                            else:
+                                pretty_definition = prettify_definition(definition)
+                                pretty_quickdef = prettify_quickdef(quickdef)
+                                if len(words) > 0:
+                                    submission.reply(format_reply(pretty_quickdef, pretty_definition))
+                                    print("Bot replying to : {}", submission.title)
+                                    posts_replied_to.append(submission.id)
+                    except:
+                      print("No useable submissions found.")
     else:
         print("ERROR: Debug parameter badly set.")
 
